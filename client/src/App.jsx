d4848386bcee2679314fa11392710f58c8a9be5b
sohal13 <sohalr1305@gmail.com>
2023-12-23 11:44:55 +0530

    new updts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,7 +22,7 @@ import About from "./pages/About"
 function App() {
 
   return (
-    <div className=" bg-green-200 w-full h-screen">
+    <div className=" bg-green-200 w-full sm:h-full h-screen">
       <BrowserRouter>
         <Header />
         <Routes>
